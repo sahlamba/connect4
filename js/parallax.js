cdf7205f5f2f58413efce01d49675c82dfc7f631
Aman Shrivastava <aman.srivastava999@gmail.com>
2014-08-01 21:21:50 +0530

    Changed parallax code, still doesn't work

--- js/parallax.js
+++ js/parallax.js
@@ -1,18 +1,13 @@
 /*global $, window*/
 
-$(document).ready(function () {
-	"use strict";
-	
-	function parallaxScroll() {
-		var scrollY = $(window).scrollTop();
-		
-		$('#prllx-layer1').css('top', '-' + ((scrollY + 0.4)) + 'px');
-		$('#prllx-layer2').css('top', '-' + ((scrollY + 0.6)) + 'px');
-		$('#prllx-layer3').css('top', '-' + ((scrollY + 0.8)) + 'px');
-		$('#prllx-layer4').css('top', '-' + ((scrollY + 1)) + 'px');
-	}
-	
-	$(window).bind('scroll', function (e) {
-		parallaxScroll();
-	});
-});
+function parallaxScroll() {
+    "use strict";
+    
+    var scrolledY = $(window).scrollTop();
+
+    $('#prllx-layer1').css('top', (-(scrolledY * 0.4)) + 'px');
+    $('#prllx-layer2').css('top', (-(scrolledY * 0.6)) + 'px');
+    $('#prllx-layer3').css('top', (-(scrolledY * 0.8)) + 'px');
+    $('#prllx-layer4').css('top', (-(scrolledY)) + 'px');
+}
+
