c17381d3a390f7ffb41588e78de80052399a06bc
Sahil Lamba <sahil.lamba95@gmail.com>
2014-08-02 19:32:17 +0530

    Parallax done, Social links done, Effects on dev page done

--- js/parallax.js
+++ js/parallax.js
@@ -1,13 +1,39 @@
 /*global $, window*/
 
-function parallaxScroll() {
-    "use strict";
-    
-    var scrolledY = $(window).scrollTop();
+$(document).ready(function () {
+	"use strict";
+	
+	$('div[data-type="prllx-layer"').each(function () {
+		var $layer = $(this);
+		
+		$(window).scroll(function () {
+			var yPos = -($(window).scrollTop() / $layer.data('speed')), coords = '50% ' + yPos + 'px', down = $('.scroll-down-instruction'), up = $('.to-top');
+			
+			$layer.css({backgroundPosition: coords});
+			
+			if ($(window).scrollTop() > 300) {
+				down.fadeOut(100);
+				up.fadeIn(300);
+			} else {
+				up.fadeOut(100);
+				down.fadeIn(300);
+			}
+			
+		});
+		
+		$('a[href="#top"]').on('click', function (e) {
+			e.preventDefault();
 
-    $('#prllx-layer1').css('top', (-(scrolledY * 0.4)) + 'px');
-    $('#prllx-layer2').css('top', (-(scrolledY * 0.6)) + 'px');
-    $('#prllx-layer3').css('top', (-(scrolledY * 0.8)) + 'px');
-    $('#prllx-layer4').css('top', (-(scrolledY)) + 'px');
-}
+			var target = this.hash,
+				$target = $(target);
 
+			$('html, body').stop().animate({
+				'scrollTop': $target.offset().top
+			}, 1000, 'swing', function () {
+				window.location.hash = target;
+			});
+		});
+		
+	});
+	
+});
