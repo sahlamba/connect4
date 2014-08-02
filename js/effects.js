919b3964380432ccedce7a0de256a43c5f023d96
Sahil Lamba <sahil.lamba95@gmail.com>
2014-08-02 19:52:50 +0530

    Changed parallax speeds, Improved rules functionality

--- js/effects.js
+++ js/effects.js
@@ -4,7 +4,7 @@ function reload() {
 	"use strict";
 	
 	location.reload();
-	
+
 	$(window).bind('beforeunload', function () {
 		return 'Your game progress will not be saved.';
 	});
@@ -14,18 +14,29 @@ var rules_light_box_flag = 0;
 
 function viewRules() {
 	"use strict";
-	
+
 	if (rules_light_box_flag === 0) {
-		$("#rules-lightbox").css("display", "block");
+		$("#rules-lightbox").fadeIn(200);
 		rules_light_box_flag = 1;
 	}
 }
 
 function hideRules() {
 	"use strict";
-	
+
 	if (rules_light_box_flag === 1) {
-		$("#rules-lightbox").css("display", "none");
+		$("#rules-lightbox").fadeOut(200);
 		rules_light_box_flag = 0;
 	}
 }
+
+$(document).ready(function () {
+	"use strict";
+
+	$(document).keyup(function (e) {
+		if (e.keyCode === 27 && rules_light_box_flag === 1) { //Esc Key
+			$("#rules-lightbox").fadeOut(200);
+			rules_light_box_flag = 0;
+		}
+	});
+});
