26c7bb592ad4679be9bc60d6c6632dad986c2571
Sahil Lamba <sahil.lamba95@gmail.com>
2014-08-11 23:02:41 +0530

    Removed parallax

--- code4/js/parallax.js
+++ code4/js/parallax.js
@@ -7,33 +7,10 @@ $(document).ready(function () {
 		var $layer = $(this);
 		
 		$(window).scroll(function () {
-			var yPos = -($(window).scrollTop() / $layer.data('speed')), coords = '50% ' + yPos + 'px', down = $('.scroll-down-instruction'), up = $('.to-top');
+			var yPos = -($(window).scrollTop() / $layer.data('speed')), coords = '50% ' + yPos + 'px';
 			
 			$layer.css({backgroundPosition: coords});
-			
-			if ($(window).scrollTop() > 300) {
-				down.fadeOut(100);
-				up.fadeIn(300);
-			} else {
-				up.fadeOut(100);
-				down.fadeIn(300);
-			}
-			
-		});
-		
-		$('a[href="#top"]').on('click', function (e) {
-			e.preventDefault();
-
-			var target = this.hash,
-				$target = $(target);
-
-			$('html, body').stop().animate({
-				'scrollTop': $target.offset().top
-			}, 1000, 'swing', function () {
-				//window.location.hash = target;
-			});
-		});
-		
+		});	
 	});
 	
 });
